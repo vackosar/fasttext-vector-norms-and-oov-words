4bd150009342579781a49d18ce9c00f4c03df7f8
vackosar <admin@vaclavkosar.com>
2019-07-01 20:56:57 +0200

    sample better ng_norm

--- main.py
+++ main.py
@@ -199,7 +199,7 @@ norms, tfs = calc_norms(no_ngram_vector)
 # sorted_idxs = matutils.argsort(norms, reverse=True)
 rnd_word_idx = [
     # sorted_idxs[400000], sorted_idxs[800000], sorted_idxs[1200000], sorted_idxs[1600000], sorted_idxs[1800000]
-    select_word_index(norms, tfs, 5_000, 10_000, 4, 4.1),
+    select_word_index(norms, tfs, 5_000, 10_000, 4.3, 4.6),
     select_word_index(norms, tfs, 70_000, 100_000, 10, 11),
     select_word_index(norms, tfs, 900_000, 1000_000, 4.7, 5),
     select_word_index(norms, tfs, 3000_000, 4000_000, 4.7, 5),
@@ -233,6 +233,15 @@ fig.show()
 
 #%% def plot_custom_vec_norms():
 norms, tfs = calc_norms(custom_vec)
+# sorted_idxs = matutils.argsort(norms, reverse=True)
+rnd_word_idx = [
+    # sorted_idxs[400000], sorted_idxs[800000], sorted_idxs[1200000], sorted_idxs[1600000], sorted_idxs[1800000]
+    select_word_index(norms, tfs, 5_000, 10_000, 2.4, 2.6),
+    select_word_index(norms, tfs, 70_000, 100_000, 10, 11),
+    select_word_index(norms, tfs, 900_000, 1000_000, 2.4, 2.6),
+    select_word_index(norms, tfs, 3000_000, 4000_000, 2.4, 2.6),
+    select_word_index(norms, tfs, 15_000_000, 17_000_000, 2.4, 2.6),
+]
 
 seaborn.set(style='white', rc={'figure.figsize': (12, 8)})
 fig: Figure = plt.figure()
@@ -246,16 +255,6 @@ ax.scatter(tfs, norms, alpha=0.6, edgecolors='none', s=5, label=fasttext_model_v
 common_words_norm, common_words_tfs = common_words_norms(custom_vec)
 ax.scatter(common_words_tfs, common_words_norm, alpha=0.8, edgecolors='none', s=5, label=mit_10k_common_label)
 
-sorted_idxs = matutils.argsort(norms, reverse=True)
-
-rnd_word_idx = [
-    sorted_idxs[400000], sorted_idxs[800000], sorted_idxs[1200000], sorted_idxs[1600000], sorted_idxs[1800000]
-    # select_word_index(70_000, 100_000, 2.4, 2.71),
-    # select_word_index(55_000, 70_000, 0.53, 0.6),
-    # select_word_index(4600_000, 7000_000, 0.44, 0.47),
-    # select_word_index(4600_000, 7000_000, 1.26, 1.3),
-    # select_word_index(4600_000, 7000_000, 2.4, 2.71)
-]
 for i in rnd_word_idx:
     word = wv.index2word[i]
     tf = wv.vocab[word].count
